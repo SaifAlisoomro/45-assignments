bc1b25cb56b127e73e46d1c102b7c63b7df7d3bd
SaifAlisoomro <hk4948171@gmail.com>
2024-03-30 20:23:59 +0500

    first

---
+++ greeting/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let members = ['Hafeez', 'sulleman', 'haseeb', 'saif', 'soomro'];
+let message = 'would you like to eat apple ,';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ greeting/app.ts
@@ -0,0 +1,6 @@
+let members: string[] = ['Hafeez','sulleman','haseeb','saif','soomro'];
+let message: string= 'would you like to eat apple ,';
+for (let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+    
+}
